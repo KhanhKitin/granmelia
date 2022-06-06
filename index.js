a251fd1c201481a04ed0a48e4adac0f6ee61c314
Khánh kitin <kitinkhanh@gmail.com>
2022-06-06 23:14:51 +0700

    update code

---
+++ index.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  $('.slide-show').slick({
+    dots: false,
+    prevArrow: "<div class='custom-arrow-prev a-left control-c prev slick-prev'><img src='image/arrow-prev.png'></div>",
+    nextArrow: "<div class='custom-arrow-next a-right control-c next slick-next'><img src='image/arrow-next.png'></div>",
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          dots: false
+        }
+      }
+    ]
+  });
+});
